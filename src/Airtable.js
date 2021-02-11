8a05243c613e8c77f6b9612e7f2c7762fb30d5c7
beata <beata@emc2wave.com>
2021-02-10 20:55:22 -0800

    set State get data

--- src/Airtable.js
+++ src/Airtable.js
@@ -4,6 +4,14 @@ import axios from 'axios';
 const url = '/api/products';
 
 const Airtable = () => {
+  const [products, setProducts] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const { data } = await axios.get(url);
+    } catch (error) {}
+  };
+
   return <div>hello from airtable</div>;
 };
 
