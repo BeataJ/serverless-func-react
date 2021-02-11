84c9e226cc3f3c4af3ac2b446d450e82819a0c57
beata <beata@emc2wave.com>
2021-02-10 20:47:20 -0800

    set useEffect useState axios

--- src/Airtable.js
+++ src/Airtable.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const url = '/api/products';
 
 const Airtable = () => {
   return <div>hello from airtable</div>;
